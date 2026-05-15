e04a92fc217a587f5fd86598e995ba53cf867033
minh <sktt1minh2003@gmail.com>
2026-05-15 16:38:48 +0700

    fix mailto

--- backend/src/config/sendMail.js
+++ backend/src/config/sendMail.js
@@ -3,21 +3,27 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sendMail = async (to, subject, text, html) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USENAME,
-            pass: process.env.EMAIL_PASSWORD
-        },
-    });
-    const mailOptions = {
-        from: process.env.EMAIL_USENAME,
-        to,
-        subject,
-        text,
-        html
-    };
-    await transporter.sendMail(mailOptions);
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            family : 4,
+            auth: {
+                user: process.env.EMAIL_USENAME,
+                pass: process.env.EMAIL_PASSWORD
+            },
+        });
+        const mailOptions = {
+            from: process.env.EMAIL_USENAME,
+            to,
+            subject,
+            text,
+            html
+        };
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.error("Error sending email:", error);
+    }
+
 };
 
 export default sendMail;
