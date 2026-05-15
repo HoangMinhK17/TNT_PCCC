7981db650fb91bb3b2a9e63b05cfbe5ef7b3388e
minh <sktt1minh2003@gmail.com>
2026-05-15 16:30:17 +0700

    fix_email

--- backend/src/config/sendMail.js
+++ backend/src/config/sendMail.js
@@ -4,15 +4,10 @@ dotenv.config();
 
 const sendMail = async (to, subject, text, html) => {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USENAME,
             pass: process.env.EMAIL_PASSWORD
-        },
-        tls: {
-            rejectUnauthorized: false,
         },
     });
     const mailOptions = {
