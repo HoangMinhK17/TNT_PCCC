fe2cb586a63f38ac28d898bb1de705307823439e
minh <sktt1minh2003@gmail.com>
2026-05-18 08:42:49 +0700

    fix_mailToIPV4

--- backend/src/config/sendMail.js
+++ backend/src/config/sendMail.js
@@ -5,8 +5,10 @@ dotenv.config();
 const sendMail = async (to, subject, text, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
-            family : 4,
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            family: 4,
             auth: {
                 user: process.env.EMAIL_USENAME,
                 pass: process.env.EMAIL_PASSWORD
